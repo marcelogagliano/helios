05c8159eb582270d84e24be649a10cee6c4b45a3
Marcelo Gagliano <marcelo.gagliano@gmail.com>
2018-10-09 23:43:24 -0400

    redo-commit

--- crawlerProcess.js
+++ crawlerProcess.js
@@ -10,6 +10,9 @@ var nounSet = new Set();
 var verbSet = new Set();
 var topicSet = new Set();
 var peopleSet = new Set();
+var placeSet = new Set();
+var organizationSet = new Set();
+var knowledgeSet = new Set();
 var urlSet = new Set();
 const mongoose = require('mongoose');
 const dev_db_url = 'mongodb://heliosapp:admin2018@ds045622.mlab.com:45622/helios';
@@ -40,48 +43,78 @@ function parseData(result) {
         var content = $(this).prev();
         var item = content.text().replace(/\n/g, '').replace(/ *\[[^\]]*]+/, '').split(/[.?!]/);
 
-        for (var k = 0; k < element.length; k++)
-            if (item[k].indexOf(" ") > 0)
-                transitionArray.push(element[k]);
+        for (var k = 0; k < item.length; k++) {
+            if (item[k].includes(" ")) {
+                transitionArray.push(item[k]);
+            }
+        }
     });
 
 
     $('a').each(function (i, link) {
         var urlAddress = new String($(link).attr('href'));
 
-        if(urlAddress.includes("https://en.wikipedia.org")){
-            urlSet.add(urlAddress.replace("String",""));
+        if (urlAddress.includes("https://en.wikipedia.org")) {
+            urlSet.add(urlAddress.replace("String", ""));
         }
-        if(urlAddress.startsWith("/wiki")){
-            urlSet.add("https://en.wikipedia.org" + urlAddress.replace("String",""));
+        if (urlAddress.startsWith("/wiki")) {
+            urlSet.add("https://en.wikipedia.org" + urlAddress.replace("String", ""));
         }
     });
 
+
     for (var j = 0; j < transitionArray.length; j++) {
-        var nounSubArray = nlp(transitionArray[j]).nouns().normalize().out('array');
-        var verbSubArray = nlp(transitionArray[j]).verbs().normalize().out('array');
-        var topicSubArray = nlp(transitionArray[j]).topics().normalize().out('array');
-        var peopleSubArray = nlp(transitionArray[j]).people().normalize().out('array');
+        var sentenceNLP = nlp(transitionArray[j]);
+        //console.log(sentenceNLP + " -->  " + sentenceNLP.sentences().isQuestion());
 
-        if (verbSubArray.length > 0) {
-            for (var a = 0; a < nounSubArray.length; a++)
-                nounSet.add(nounSubArray[a].replace(/ *\[[^\]]*]/, ''));
+        if (sentenceNLP.sentences().isQuestion().length < 1) {
+            var nounSubArray = sentenceNLP.nouns().normalize().out('array');
+            var verbSubArray = sentenceNLP.verbs().normalize().out('array');
+            var topicSubArray = sentenceNLP.topics().normalize().out('array');
+            var peopleSubArray = sentenceNLP.people().normalize().out('array');
+            var placeSubArray = sentenceNLP.places().normalize().out('array');
+            var organizationSubArray = sentenceNLP.organizations().normalize().out('array');
 
-            for (var b = 0; b < verbSubArray.length; b++)
-                verbSet.add(verbSubArray[b].replace(/ *\[[^\]]*]/, ''));
+            if (verbSubArray.includes("is")) {
+                //console.log(verbSubArray);
+                //console.log(transitionArray[j]);
+                if ((topicSubArray.length > 0) || (peopleSubArray.length > 0)) {
+                    //console.log(peopleSubArray + " vs " + topicSubArray);
 
-            for (var c = 0; c < topicSubArray.length; c++)
-                topicSet.add(topicSubArray[c].replace(/ *\[[^\]]*]/, ''));
+                    for (var a = 0; a < nounSubArray.length; a++)
+                        nounSet.add(nounSubArray[a].replace(/ *\[[^\]]*]/, ''));
 
-            for (var d = 0; d < peopleSubArray.length; d++)
-                peopleSet.add(peopleSubArray[d].replace(/ *\[[^\]]*]/, ''));
+                    for (var b = 0; b < verbSubArray.length; b++)
+                        verbSet.add(verbSubArray[b].replace(/ *\[[^\]]*]/, ''));
 
-            sentenceArray.push(transitionArray[j]);
+                    for (var c = 0; c < topicSubArray.length; c++)
+                        topicSet.add(topicSubArray[c].replace(/ *\[[^\]]*]/, ''));
+
+                    for (var d = 0; d < peopleSubArray.length; d++)
+                        peopleSet.add(peopleSubArray[d].replace(/ *\[[^\]]*]/, ''));
+
+                    for (var e = 0; e < placeSubArray.length; e++)
+                        placeSet.add(placeSubArray[e].replace(/ *\[[^\]]*]/, ''));
+
+                    for (var f = 0; f < organizationSubArray.length; f++)
+                        organizationSet.add(organizationSubArray[f].replace(/ *\[[^\]]*]/, ''));
+
+                    sentenceArray.push(transitionArray[j]);
+
+
+                    var knowledgeNode = {
+                        sentence: transitionArray[j],
+                        source: "",
+                        target: ""
+                    };
+                }
+
+            }
         }
     }
     //sentenceArray = transitionArray;
-    console.log(urlSet);
-    //console.log([sentenceArray, nounSet, verbSet, topicSet, peopleSet, urlSet]);
+    //console.log(urlSet);
+    //console.log([sentenceArray, nounSet, verbSet, topicSet, peopleSet, placeSet, organizationSet,urlSet]);
 }
 
 function instantiateEntity(id, name, target, category) {
@@ -93,7 +126,7 @@ function instantiateEntity(id, name, target, category) {
     });
     // Set up mongoose connection
     //https://mlab.com/databases/helios/collections/infonodes
-    
+
 
     mongoose.connect(mongoDB, {
         useNewUrlParser: true
@@ -110,21 +143,23 @@ function instantiateEntity(id, name, target, category) {
     });
 }
 
-function retrieveEntity(){
+function retrieveEntity() {
     //var mongoose = require('mongoose');
     //var dev_db_url = 'mongodb://heliosapp:admin2018@ds045622.mlab.com:45622/helios';
     //var mongoDB = process.env.MONGODB_URI || dev_db_url;
-    
+
     var MongoClient = require('mongodb').MongoClient;
 
-    MongoClient.connect(dev_db_url, { useNewUrlParser: true }, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("helios");
-      dbo.collection("infonodes").findOne({}, function(err, result) {
+    MongoClient.connect(dev_db_url, {
+        useNewUrlParser: true
+    }, function (err, db) {
         if (err) throw err;
-        console.log(result);
-        db.close();
-      });
+        var dbo = db.db("helios");
+        dbo.collection("infonodes").findOne({}, function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+        });
     });
 }
 
